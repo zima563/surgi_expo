7ae4b5a29825903de231f393207258daa76b6c56
zima563 <mohamedabdelazim654@gmail.com>
2024-10-16 10:42:14 +0300

    redis cash

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const dbConnection = require('./DB/dbConnection');
 const { bootstrap } = require('./src/index.routes');
 const { login } = require('./src/modules/user/user.controller');
+const compression = require('compression')
 
 const app = express()
 const port = 3000
@@ -13,6 +14,7 @@ dotenv.config();
 
 dbConnection();
 app.use("/", express.static("uploads"));
+app.use(compression());
 app.use(express.json())
 app.use(cors())
 
