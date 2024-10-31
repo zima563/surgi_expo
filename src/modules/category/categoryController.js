ef8fb1487e26a22ca2e851dd9c8759cc44a2e2ee
zima563 <mohamedabdelazim654@gmail.com>
2024-10-31 16:45:09 +0300

    anding select in category

--- src/modules/category/categoryController.js
+++ src/modules/category/categoryController.js
@@ -1,7 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const categoryModel = require("../../../DB/model/category.model");
 const prisma = new PrismaClient();
-// const redisClient = require("../../../DB/redis");
 const catchError = require("../../middlewares/catchError");
 const ApiFeatures = require("../../utils/apiFeatures");
 const { addOne, getOne, updateOne, deleteOne } = require("../handlers/handler");
@@ -15,56 +14,23 @@ const getCategories = catchError(async (req, res, next) => {
     .search("category")
     .limitedFields();
 
-  // Get the count for pagination
   const countDocuments = await prisma.category.count({
-    where: { parentId: null, ...apiFeatures.prismaQuery.where }, // Include the where conditions
+    where: { parentId: null, ...apiFeatures.prismaQuery.where },
   });
 
-  await apiFeatures.paginateWithCount(countDocuments); // Call paginate after getting the count
-
-  // Execute the query for the documents
-  const categories = await apiFeatures.exec();
-
-  // Create the response object
-  const response = {
-    paginationResult: apiFeatures.paginationResult, // Include pagination details
-    categories: categories.result.map(category => ({
-      ...category,
-      image: process.env.MEDIA_BASE_URL + category.image, // Update image URL
-    })),
-  };
-
-  res.json(response);
-});
-
-const getSubCategories = catchError(async (req, res, next) => {
-  // Parse parentId as an integer or null if "null" is provided
-  const parentId = req.params.parentId === 'null' ? null : parseInt(req.params.parentId, 10);
-  // Initialize ApiFeatures with parentId from `req.params.id`
-  let apiFeatures = new ApiFeatures(prisma.category, { ...req.query, parentId })
-    .filter()
-    .sort()
-    .search("category")
-    .limitedFields();
-
-
-
-  // Get the count of documents that match the `parentId`
-  const countDocuments = await prisma.category.count({
-    where: { parentId, ...apiFeatures.prismaQuery.where },
-  });
-
-
-  // Use the count for pagination
   await apiFeatures.paginateWithCount(countDocuments);
 
-  // Execute the query to get categories
-  const categories = await apiFeatures.exec();
+  const categories = await prisma.category.findMany({
+    where: apiFeatures.prismaQuery.where,
+    select: {
+      categoryId: true, // Include categoryId
+      ...apiFeatures.prismaQuery.select,
+    },
+  });
 
-  // Prepare the response
   const response = {
     paginationResult: apiFeatures.paginationResult,
-    categories: categories.result.map(category => ({
+    categories: categories.map(category => ({
       ...category,
       image: process.env.MEDIA_BASE_URL + category.image,
     })),
@@ -73,7 +39,39 @@ const getSubCategories = catchError(async (req, res, next) => {
   res.json(response);
 });
 
+const getSubCategories = catchError(async (req, res, next) => {
+  const parentId = req.params.parentId === 'null' ? null : parseInt(req.params.parentId, 10);
 
+  let apiFeatures = new ApiFeatures(prisma.category, { ...req.query, parentId })
+    .filter()
+    .sort()
+    .search("category")
+    .limitedFields();
+
+  const countDocuments = await prisma.category.count({
+    where: { parentId, ...apiFeatures.prismaQuery.where },
+  });
+
+  await apiFeatures.paginateWithCount(countDocuments);
+
+  const categories = await prisma.category.findMany({
+    where: apiFeatures.prismaQuery.where,
+    select: {
+      categoryId: true, // Include categoryId
+      ...apiFeatures.prismaQuery.select,
+    },
+  });
+
+  const response = {
+    paginationResult: apiFeatures.paginationResult,
+    categories: categories.map(category => ({
+      ...category,
+      image: process.env.MEDIA_BASE_URL + category.image,
+    })),
+  };
+
+  res.json(response);
+});
 
 const getCategory = getOne(prisma.category);
 
