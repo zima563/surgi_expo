6f2f9a4e286fa9e8eb2b48bbda1c1f7cabc6a965
zima563 <mohamedabdelazim654@gmail.com>
2024-11-04 11:41:53 +0200

    updated files

--- prisma/migrations/20241104094141_update/migration.sql
+++ prisma/migrations/20241104094141_update/migration.sql
@@ -5,8 +5,9 @@ CREATE TABLE `category` (
     `slug` VARCHAR(100) NOT NULL,
     `image` VARCHAR(191) NOT NULL,
     `description` VARCHAR(1000) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `parentId` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
